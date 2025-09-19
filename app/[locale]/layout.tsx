a9010e70956d863adf36ab949c220975b23e041d
Ali-Fayzullaev <alifayzullaev2020@gmail.com>
2025-09-19 10:51:49 +0500

    added Yandex.Metrika

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -31,6 +31,38 @@ export default async function LocaleLayout({
 
   return (
     <Providers locale={locale} messages={messages}>
+      {/* Yandex.Metrika */}
+      <Script id="yandex-metrika" strategy="afterInteractive">
+        {`
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+              if (document.scripts[j].src === r) { return; }
+            }
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104202860', 'ym');
+
+          ym(104202860, 'init', {
+            ssr: true,
+            webvisor: true,
+            clickmap: true,
+            ecommerce: "dataLayer",
+            accurateTrackBounce: true,
+            trackLinks: true
+          });
+        `}
+      </Script>
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/104202860"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+
       {/* Meta Pixel */}
       <Script id="meta-pixel" strategy="afterInteractive">
         {`
